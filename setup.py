f48944a777c5b61bb6dc3a8ad2f1ef9328a79b10
Joshua Phillips <josh.phillips@aparavi.com>
2025-07-26 13:39:43 -0700

    Bumped version.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if os.path.exists(req_path):
 
 setup(
     name="aparavi-dtc-sdk",
-    version="0.0.7",
+    version="0.0.8",
     author="Aparavi",
     author_email="support@aparavi.com",
     description="Python SDK for Aparavi Data Toolchain API",
